df2db2fe1feb501ae978bb23272f0cf39e325f63
rdalla <rodvfilho@terra.com.br>
2020-03-28 10:21:13 -0300

    Deleted unused files.

--- include/ledtask.h
+++
@@ -1,7 +0,0 @@
-#ifndef LEDTASK_H
-#define LEDTASK_H
-typedef enum {OFF, ON, BLINK} led_state_t;
-extern void init_leds_task();
-extern void change_led_state(int led, char *param);
-extern void blink_led(int led, int delay);
-#endif
